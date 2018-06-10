0af013040b266898ca48374590192e22f7ad75eb
Diego Hernandes <diego@hernandev.com>
2018-06-09 23:09:10 -0300

    babel & eslint configuration

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  'extends': 'standard',
+  'rules': {
+    'import/first': 0,
+    'import/named': 2,
+    'import/namespace': 2,
+    'import/default': 2,
+    'import/export': 0,
+    'import/extensions': 0,
+    'import/no-unresolved': 0,
+    'import/no-extraneous-dependencies': 0,
+    'import/no-webpack-loader-syntax': 0
+  }
+}
